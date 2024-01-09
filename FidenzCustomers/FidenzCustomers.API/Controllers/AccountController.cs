57a133966f7b3cf60ffab6db40df401329ebd91e
Sadeepa Dhananjaya <sadeepaekanayaked@gmail.com>
2024-01-09 14:48:30 +0530

    Error Handling

--- FidenzCustomers/FidenzCustomers.API/Controllers/AccountController.cs
+++ FidenzCustomers/FidenzCustomers.API/Controllers/AccountController.cs
@@ -24,7 +24,7 @@ namespace FidenzCustomers.API.Controllers
         private readonly RoleManager<IdentityRole> _roleManager;
         private IConfiguration _config;
 
-        public AccountController( UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager, IConfiguration config)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager, IConfiguration config)
         {
             _userManager = userManager;
             _signInManager = signInManager;
@@ -34,97 +34,121 @@ namespace FidenzCustomers.API.Controllers
         [HttpPost("[action]")]
         public async Task<IActionResult> Login([FromBody] UserDto user)
         {
-            if (ModelState.IsValid)
+            try
             {
-                var result = await _signInManager.PasswordSignInAsync(user.Email, user.Password, false, lockoutOnFailure: false);
-                if (result.Succeeded)
+
+                if (ModelState.IsValid)
                 {
-                    var userDB = _userManager.FindByEmailAsync(user.Email).Result;
-                    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-                    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-                    var claims = new[]
+                    var result = await _signInManager.PasswordSignInAsync(user.Email, user.Password, false, lockoutOnFailure: false);
+                    if (result.Succeeded)
                     {
+                        var userDB = _userManager.FindByEmailAsync(user.Email).Result;
+                        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+                        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+                        var claims = new[]
+                        {
                             new Claim(JwtRegisteredClaimNames.Email, userDB.Email),
                             new Claim(JwtRegisteredClaimNames.Sub, userDB.UserName),
                             new Claim(SD.Role_Admin, _userManager.IsInRoleAsync(userDB, SD.Role_Admin).Result ? "true" : "false",ClaimValueTypes.Boolean),
                         };
 
-                    var tokenDescriptor = new SecurityTokenDescriptor
-                    {
-                        Subject = new ClaimsIdentity(claims),
-                        Expires = DateTime.Now.AddMinutes(30),
-                        SigningCredentials = credentials,
-                        Issuer = _config["Jwt:Issuer"],
-                        Audience = _config["Jwt:Audience"]
-                    };
+                        var tokenDescriptor = new SecurityTokenDescriptor
+                        {
+                            Subject = new ClaimsIdentity(claims),
+                            Expires = DateTime.Now.AddMinutes(30),
+                            SigningCredentials = credentials,
+                            Issuer = _config["Jwt:Issuer"],
+                            Audience = _config["Jwt:Audience"]
+                        };
 
-                    var token = new JwtSecurityTokenHandler().CreateToken(tokenDescriptor);
-                    var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
-                    return Ok(new { token = tokenString });
+                        var token = new JwtSecurityTokenHandler().CreateToken(tokenDescriptor);
+                        var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                        return Ok(new { token = tokenString });
+                    }
+                    else
+                    {
+                        return BadRequest("Invalid Credentials");
+                    }
                 }
-                else
-                {
-                    return BadRequest("Invalid Credentials");
-                }
+                return StatusCode(StatusCodes.Status500InternalServerError);
             }
-            return StatusCode(StatusCodes.Status500InternalServerError);
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
         [HttpPost("[action]")]
         public async Task<IActionResult> Register([FromBody] UserRegisterationDto registerUser)
         {
-            if (ModelState.IsValid)
+            try
             {
-                ApplicationUser user = new()
+                if (ModelState.IsValid)
                 {
-                    UserName = registerUser.Email,
-                    Email = registerUser.Email,
-                    Name = registerUser.Name,
-                    CreatedAt = DateTime.Now,
-                };
-                var result = await _userManager.CreateAsync(user, registerUser.Password);
-                if (result.Succeeded)
-                {
-                    if (!string.IsNullOrEmpty(registerUser.Role))
+                    ApplicationUser user = new()
                     {
-                        await _userManager.AddToRoleAsync(user, registerUser.Role);
-                    }
-                    else
+                        UserName = registerUser.Email,
+                        Email = registerUser.Email,
+                        Name = registerUser.Name,
+                        CreatedAt = DateTime.Now,
+                    };
+                    var result = await _userManager.CreateAsync(user, registerUser.Password);
+                    if (result.Succeeded)
                     {
-                        await _userManager.AddToRoleAsync(user, SD.Role_Customer);
+                        if (!string.IsNullOrEmpty(registerUser.Role))
+                        {
+                            await _userManager.AddToRoleAsync(user, registerUser.Role);
+                        }
+                        else
+                        {
+                            await _userManager.AddToRoleAsync(user, SD.Role_Customer);
+                        }
+                        await _signInManager.SignInAsync(user, isPersistent: false);
                     }
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                }
 
-                foreach (var error in result.Errors)
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                        return BadRequest(error.Description);
+                    }
+
+                }
+                else
                 {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                    return BadRequest(error.Description);
+                    return BadRequest(ModelState);
                 }
-
+                return StatusCode(StatusCodes.Status201Created);
             }
-            else
+            catch (Exception ex)
             {
-                return BadRequest(ModelState);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
-            return StatusCode(StatusCodes.Status201Created);
+
         }
 
         [Authorize]
         [HttpGet("[action]")]
         public IActionResult GetUser()
         {
-            var accessToken = Request.Headers[HeaderNames.Authorization];
-            if (accessToken.Count == 0)
+            try
             {
-                return BadRequest("Invalid Token");
+                var accessToken = Request.Headers[HeaderNames.Authorization];
+                if (accessToken.Count == 0)
+                {
+                    return BadRequest("Invalid Token");
+                }
+                var token = accessToken.ToString().Split(" ")[1];
+                var handler = new JwtSecurityTokenHandler();
+                var jsonToken = handler.ReadToken(token);
+                var tokenS = handler.ReadToken(token) as JwtSecurityToken;
+                var email = tokenS.Claims.First(claim => claim.Type == "email").Value;
+                var user = _userManager.FindByEmailAsync(email).Result;
+                return Ok(user);
             }
-            var token = accessToken.ToString().Split(" ")[1];
-            var handler = new JwtSecurityTokenHandler();
-            var jsonToken = handler.ReadToken(token);
-            var tokenS = handler.ReadToken(token) as JwtSecurityToken;
-            var email = tokenS.Claims.First(claim => claim.Type == "email").Value;
-            var user = _userManager.FindByEmailAsync(email).Result;
-            return Ok(user);
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+
         }
 
 
